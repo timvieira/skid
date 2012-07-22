5963a2010f6457c04a132bff9999c5d347079d23
timv <tim.f.vieira@gmail.com>
2012-07-22 15:07:48 -0400

    robust downloading for http documents.
    import from delicious

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
+"""
+TODO:
+
+ - urls with a glob in them get expanded..
+   e.g.  http://web.archive.org/web/*/http://www.srcf.ucam.org/~hmw26/join-the-dots/
+
+ - convert a query with the term #my-tag to tags:my-tag
+
+ - update entry (instead of ignore with warning) existing paths in database
+"""
+
 import os
 from datetime import datetime
 from glob import glob
@@ -12,10 +23,10 @@ from debug import ip
 import re
 from terminal import yellow, red, blue, green
 from fsutils import secure_filename
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from pdfutils.conversion import pdftotext
 from iterextras import iterview
-
+from web.download import download
 
 # globals
 DIRECTORY = 'data~'
@@ -23,11 +34,13 @@ NAME = 'index'
 CACHE = 'cache~'
 
 def create():
-    """ Create a new database. """
+    """ Create a new Whoosh index.. """
     print 'creating new database in directory %s' % DIRECTORY
     os.system('rm -rf %s' % DIRECTORY)
     os.mkdir(DIRECTORY)
-    schema = Schema(path = ID(stored=True),
+    schema = Schema(path = ID(stored=True, unique=True),
+                    title = TEXT(stored=True),
+                    description = TEXT(stored=True),
                     text = TEXT(stored=True),
                     tags = KEYWORD(stored=True))
     create_in(DIRECTORY, schema, NAME)
@@ -40,12 +53,10 @@ if not os.path.exists(DIRECTORY):
 if not os.path.exists(CACHE):
     os.mkdir(CACHE)
 
-
+# handle to Whoosh index
 ix = open_dir(DIRECTORY, NAME)
 
-
-# TODO: do we want to convert a query with the term #my-tag to tags:my-tag?
-def search(q):
+def _search(q):
     q = unicode(q.decode('utf8'))
     with ix.searcher() as searcher:
         qp = QueryParser('text', schema=ix.schema)
@@ -54,24 +65,48 @@ def search(q):
         for hit in searcher.search(q):
             yield hit
 
+def search(q):
+    print
+    print 'query:', q
+    for hit in _search(q):
+        print 'docnum:', hit.docnum
+        print 'title:', hit['title']
+        print 'path:', hit['path']
+        print 'tags:', hit['tags']
+        #print '\n'.join('%s: %s' % (k, hit[k]) for k in hit.fields() if k != 'parse')
+        print
+    print
+
 
 def warn(x):
     print '[%s]' % yellow % 'warn', x
 
 
+def import_document(location, tags, title='', description=''):
 
-
-def import_document(location, tags):
+    print blue % 'adding %s' % location
 
     if isinstance(tags, basestring):
-        tags = re.split(',\s*', tags)
+        tags = tags.split()
+
+    # handle directories os.path.isdir
+    # expand ~
+    # source code: python, java, scala
+    # plain text
+
+    with ix.searcher() as searcher:
+        results = searcher.find('path', unicode(location))
+        if len(results) != 0:
+            print results
+            warn('document already added.')
+            return
 
     if os.path.exists(location):         # is this something on disk?
-        tags.append('ondisk')
+        tags.append('$local')
         filename = location              # leave the file where it is, no caching
 
     elif location.startswith('http'):    # cache links
-        tags.append('link')
+        tags.append('$url')
 
         # cache file
         filename = os.path.join(CACHE, secure_filename(location))
@@ -79,30 +114,55 @@ def import_document(location, tags):
         if os.path.exists(filename):
             warn('file %s already cached at %s.' % (location, filename))
 
-        content = urlopen(location).read()
-        with file(filename, 'wb') as f:
-            f.write(content)
-
-        tags.append('cached')
-
+        try:
+            content = download(location, tries=3, usecache=True, cachedir='cache~')
+            with file(filename, 'wb') as f:
+                f.write(content)
+        except (KeyboardInterrupt, URLError):
+            tags.append('$failed-to-cache')
+        else:
+            tags.append('$cached')
 
     if filename.endswith('.pdf'):       # extract text from pdfs
         text = pdftotext(filename)
 
     else:
-        with file(filename, 'r') as f:
-            text = f.read()
+        if os.path.exists(filename):
 
-        if filename.endswith('.html'):  # clean up html
-            text = re.sub('<.*?>', '', content)
+            with file(filename, 'r') as f:
+                text = f.read()
 
-    # sha1 hash page
+            if filename.endswith('.html'):  # clean up html
+                text = re.sub('<.*?>', '', content)
+
+        else:
+            text = '-*- missing -*-'
+
+    # sha1 hash of text or content
+
+    try:
+        text = unicode(text.decode('utf8'))
+    except:
+        text = unicode(text.decode('latin1'))
 
     with ix.writer() as w:
         w.add_document(path = unicode(location),
-                       text = unicode(text.decode('utf8')),
+                       title = unicode(title or location),
+                       description = unicode(description or '-*- missing -*-'),
+                       text = text,
                        tags = unicode(' '.join(tags)))
 
+
+def add_delicious():
+    from BeautifulSoup import BeautifulSoup
+    with file('delicious_timvieira.xml') as f:
+        soup = BeautifulSoup(f)
+        for post in iterview(soup.findAll('post')):
+            print
+            import_document(location = post['href'],
+                            tags = post['tag'],
+                            title = post['description'],
+                            description = post['extended'])
 
 def add_pdfs():
     for filename in iterview(glob('/home/timv/projects/document-explorer/data/*.pdf')):
@@ -120,17 +180,5 @@ def lexicon(field):
 
 
 if __name__ == '__main__':
-
-    _search = search
-    def search(q):
-        print
-        print 'query:', q
-        for hit in _search(q):
-            print 'docnum:', hit.docnum
-            print 'path:', hit['path']
-#            print '\n'.join('%s: %s' % (k, hit[k]) for k in hit.fields() if k != 'parse')
-            print
-        print
-
     from automain import automain
     automain()
