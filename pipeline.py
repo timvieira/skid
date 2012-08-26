d5021acb02a534a65725e3fd0a35114cbcee7dd3
timv <tim.f.vieira@gmail.com>
2012-08-26 18:42:05 -0400

    major overhaul of the import pipeline.
     - added kdiff3 merging of existing notes.
     - hueristics for merging new and old data (old trumps null)
    
    implementation changes:
    
     - create Document class; some existing stuff moved in there.

--- pipeline.py
+++ pipeline.py
@@ -1,37 +1,23 @@
-import os
+"""
+Manage directory structure.
+
+TODO:
+
+  - pdftotext.txt should go in {cached}.d/data/
+
+"""
+import re, os, shutil
 from debug import ip
 from terminal import yellow, red, blue, green
 from fsutils import mkdir, filetype
-from pdfutils.conversion import pdftotext
 from web.download import download
 from text.utils import htmltotext, remove_ligatures, force_unicode
+from skid.config import CACHE
+from hashlib import sha1
 
+from pprint import pprint
 
-CACHE = '/home/timv/.tags/cache'
-
-
-def plaintext(filename):
-    "Extract plaintext from filename. Returns text, might cache."
-
-    # TODO: doesn't work on my urls e.g. "http://google.com" b/c there's no ext.
-    ext = os.path.splitext(filename)[1][1:]  # remove the dot
-
-    if ext == 'pdf':
-        # extract text from pdfs
-        text = pdftotext(filename, verbose=True, usecached=True)
-
-    else:
-        with file(filename, 'r') as f:
-            text = f.read()
-        text = force_unicode(text)
-
-        # clean up html
-        text = htmltotext(text)
-
-    text = remove_ligatures(text)
-
-    return text
-
+import pdfutils
 
 def cache_url(url):
     """
@@ -43,7 +29,7 @@ def cache_url(url):
 
         # TODO: we should be telling download where to store stuff
         # explicitly... right now we just both have the same convention.
-        cached = download(url, tries=3, usecache=True, cachedir=CACHE)
+        cached = download(url, tries=1, pause=0.1, timeout=10, usecache=True, cachedir=CACHE)
         if not cached:
             print 'Failed to download %s.' % url
             return
@@ -54,23 +40,48 @@ def cache_url(url):
     return cached
 
 
-def cache_document(source):
+def cache_document(src):
     "Cache a document, return filename of the cached file."
 
-    if source.startswith('http'):    # cache links
-        return cache_url(source)
+    # TODO: maybe support omitting 'http://', just guess that if its not a file
+    # name it must be a url.
 
-    elif os.path.exists(source):   # is this something on disk?
+    if src.startswith('http'):    # cache links
+        return cache_url(src)
 
-        # TODO: make symlink and .d directory inside cache. What if the .d
-        # directory exists near the file already?
-        return source
+    elif os.path.exists(src):   # is this something on disk?
 
-    else:
-        assert False
+        dest = os.path.join(CACHE, os.path.basename(src))
+
+        if os.path.exists(dest):
+            print 'File %r already exists' % dest
+            #raise Exception('File %r already exists' % dest)
+            return dest
+
+        shutil.copy2(src, dest)
+
+        print 'copy:', src, '->', dest
+
+        # TODO: What if the .d directory exists near the file already?
+        return dest
+
+    assert False
 
 
-def import_document(source, tags, title='', description=''):
+# TODO:
+#
+# - check if the directory already exists; maybe atomically write directory
+#
+# - dependencies between metadata to avoid recomputation; derived data versus
+#   input (e.g. tags inferred by the system v. provided by user).
+#
+# - if it's a pdf we should try to get a bibtex entry for it.
+#
+# - grab any metadata available from pyPdf see from pdfutils.metadata import metadata
+#
+# - check document already exists
+#
+def add(source, tags='', title='', description='', interactive=True):
     """
     Import document from ``source``. Procedure will download/cache the file,
     create a directory to store metadta.
@@ -82,46 +93,197 @@ def import_document(source, tags, title='', description=''):
 
     if isinstance(tags, basestring):
         tags = tags.split()
+    tags = list(tags)
 
-    # classify
-    if os.path.exists(source):         # is this something on disk?
-        tags.append('$local')
-    elif source.startswith('http'):    # cache links
-        tags.append('$url')
+    # todo: maybe we should put the file in a "sandbox" so it won't clobber
+    # anything.
+    try:
+        cached = cache_document(source)
+    except AssertionError as e:
+        print e
+        cached = None
 
-    cached = cache_document(source)
-
-    print 'filetype:', filetype(cached)
-    tags.append('$filetype:' + filetype(cached))
+    if not cached:  # failed to cache file
+        print 'failed to cached file'
+        return
 
     print cached
 
-    if cached:
-        tags.append('$cached')
+    d = Document(cached)
+
+    d.hash_contents()
+    d.extract_plaintext()
+
+    metadata = d.extract_metadata()
+    if title:
+        metadata['title'] = title
+    if tags:
+        # newtags will include all exisiting tags in the order the are listed in
+        # document, concatenating new tags (ignoring duplicates; in order
+        # listed).
+        newtags = metadata['tags'].strip().split()
+        existingtags = set(newtags)
+        for t in tags:
+            if t not in existingtags:
+                newtags.append(t.strip())
+        metadata['tags'] = ' '.join(newtags)
+
+    metadata['source'] = source
+    metadata['cached'] = cached
+
+    newdata = template(**metadata)
+
+    existing = d.cached + '.d/notes.org'
+    if os.path.exists(existing):
+        existingdata = file(existing).read()
     else:
-        tags.append('$failed-to-cache')
+        existingdata = None
 
-    print 'dir:', cached + '.d'
-    mkdir(cached + '.d')
+    # if new description is empty ignore this check.
 
-    def meta(name, content):
-        with file(cached + '.d/' + name, 'wb') as f:
-            if not isinstance(content, basestring):
-                content = '\n'.join(content)
+    if not existingdata:
+        d.meta('notes.org', newdata)
+    else:
+
+        if newdata.strip() != existingdata.strip():
+            tmp = '/tmp/newmetadata.org'
+            file(tmp, 'wb').write(newdata)
+
+            print yellow % '*************************'
+            pprint(metadata)
+            print yellow % '*************************'
+
+#                raise AssertionError('Merging notes failed because existing notes '
+#                                     'do not match new notes. Your intervention '
+#                                     'try adding the paper again in interactive '
+#                                     'mode.\n\n'
+#                                     'kdiff3 %s %s' % (existing, tmp))
+
+            print red % 'Need to merge descriptions...'
+            print yellow % '=================================='
+            if 0 != os.system('kdiff3 --merge %s %s --output %s' % (existing, tmp, existing)):
+                print red % 'merge aborted. keeping original, other temporarily saved to %s' % tmp
+
+                # todo: should probably delete any junk we might have done. This
+                # is probably easier to do if we work in a staging area (we
+                # might not even care to clean up after ourselves in that case).
+                raise AssertionError('merging notes failed. aborting')
+            else:
+                print yellow % 'merge successful.'
+                os.remove(existing + '.orig')  # remove kdiff's temporary file.
+
+    if interactive:
+        d.edit_notes()
+
+
+def edit(cached):
+    d = Document(cached)
+    d.edit_notes()
+
+
+class Document(object):
+
+    def __init__(self, cached):
+        self.cached = cached
+        assert os.path.exists(cached)
+        mkdir(cached + '.d')
+        mkdir(cached + '.d/data')
+        self.filetype = filetype(self.cached)
+
+    def edit_notes(self):
+        os.system('emacs -nw {self.cached}.d/notes.org'.format(self=self))
+
+    def open(self):
+        os.system('gnome-open {self.cached} 2>/dev/null'.format(self=self))
+
+    def meta(self, name, content, overwrite=False):
+        t = self.cached + '.d/' + name
+        assert overwrite or not os.path.exists(t), name + ' already exists!'
+        with file(t, 'wb') as f:
             content = force_unicode(content)
             content = content.encode('utf8')
             f.write(content)
             f.write('\n')    # new line at end of file
+        return content
 
-    if cached:
-        text = plaintext(cached)
-        meta('text', text)
-        meta('tags', tags)
-        meta('title', title)
-        meta('description', description)
-        meta('source', source)
+    def hash_contents(self):
+        h = self.cached + '.d/data/hash'
+
+        newhash = sha1(file(self.cached).read()).hexdigest()
+
+        if os.path.exists(h):
+            oldhash = file(h).read().strip()
+        else:
+            oldhash = newhash
+
+        if newhash != oldhash:
+            with file(h, 'wb') as f:
+                f.write(newhash)
+                f.write('\n')
+            return newhash
+        else:
+            return oldhash
 
 
-if __name__ == '__main__':
-    from automain import automain
-    automain()
+    def extract_metadata(self):
+
+        metadata = {'tags': '', 'title': '', 'description': ''}
+        if self.filetype.endswith('pdf'):
+
+            # use pdfutils metadata extractor.
+            pdfmeta = pdfutils.metadata(self.cached)
+            if pdfmeta:
+                if pdfmeta.get('title', None):
+                    metadata['title'] = pdfmeta.get('title').strip()
+                if pdfmeta.get('author', None):
+                    metadata['author'] = pdfmeta.get('author').strip()
+
+        # override anything automatic with user's notes file.
+
+        existing = self.cached + '.d/notes.org'
+        if os.path.exists(existing):
+            notes = file(existing).read()
+
+            # merge by overwriting automatic stuff.
+            metadata.update(re.findall('^:(\w+?):[ ]*([^\n]*?)\s*$', notes, re.MULTILINE))
+
+            [d] = re.findall('\n[^:]([\w\W]*$)', notes)
+            metadata['description'] = d.strip()
+
+        return metadata
+
+    def extract_plaintext(self):
+        "Extract plaintext from filename. Returns text, might cache."
+
+        # TODO: doesn't work on my urls e.g. "http://google.com" b/c there's no ext.
+        ext = os.path.splitext(self.cached)[1][1:]  # remove the dot
+
+        if ext == 'pdf':
+            # extract text from pdfs
+            text = pdfutils.pdftotext(self.cached, verbose=True, usecached=True)
+
+        else:
+            with file(self.cached, 'r') as f:
+                text = f.read()
+            text = force_unicode(text)
+            # clean up html
+            text = htmltotext(text)
+
+        text = remove_ligatures(text)
+
+        return self.meta('data/text', text, overwrite=True)
+
+
+def template(**kwargs):
+    newdata = TEMPLATE.format(**kwargs)
+    return force_unicode(newdata).encode('utf8')
+
+
+TEMPLATE = u"""\
+:title: {title}
+:source: {source}
+:cached: file://{cached}
+:tags: {tags}
+
+{description}
+"""
