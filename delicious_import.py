a197823d335a6e21a7f314e7c074460eb1d0bcea
timv <tim.f.vieira@gmail.com>
2012-09-05 11:58:50 -0400

    Should be called delicious export, not import. Also, caching doe not make much sense because delicious data is constantly changing.

--- delicious_import.py
+++ delicious_import.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys
 from urllib import urlopen
 from getpass import getpass
 from BeautifulSoup import BeautifulSoup
 
-def delicious_import(username, filename='delicious_{username}.xml', password=None, usecache=True):
+def delicious_export(username, filename='delicious_{username}.xml', password=None):
     """
     This is probably not the most secure way to do this because we are sending
     the password in plaintext over the wire.
@@ -13,25 +13,20 @@ def delicious_import(username, filename='delicious_{username}.xml', password=Non
 
     filename = filename and filename.format(username=username)
 
-    if usecache and os.path.exists(filename):
-        with file(filename) as f:
-            soup = BeautifulSoup(f)
-    else:
-        # API URL: https://user:passwd@api.del.icio.us/v1/posts/all
-        url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, password or getpass())
-        content = urlopen(url).read()
-        soup = BeautifulSoup(content)
+    # API URL: https://user:passwd@api.del.icio.us/v1/posts/all
+    url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, password or getpass())
+    content = urlopen(url).read()
+    soup = BeautifulSoup(content)
 
-        if filename:
-            with file(filename, 'wb') as f:
-                f.write(soup.prettify())
+    with file(filename, 'wb') as f:
+        f.write(soup.prettify())
 
     return soup
 
 
 if __name__ == '__main__':
     try:
-        x = len(delicious_import(*sys.argv[1:]).findAll('post'))
+        x = len(delicious_export(*sys.argv[1:]).findAll('post'))
         print 'retrieved', x, 'posts.'
     except TypeError:
         print sys.argv[0], 'username [outfile] [password]'
