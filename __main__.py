62a006b00dfdee5e2d77bd94f183c763fdda320a
timv <tim.f.vieira@gmail.com>
2013-01-25 22:04:21 -0500

    tweaks to search

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ from skid import add as _add
 from skid.config import ROOT, CACHE, REMOTE
 from fsutils import cd
 from subprocess import Popen, PIPE
-from terminal import red, cyan
+from terminal import red, cyan, yellow, magenta
 
 
 # TODO: I'd like to quickly check if I've added a paper before. Not sure hash
@@ -27,6 +27,33 @@ def add(source):
     Add document from source. Sources can be urls or filenames.
     """
     return _add.document(source, interactive=True)
+
+
+def captive():
+
+    from skid.index import open_dir, MultifieldParser, DIRECTORY, NAME
+
+    ix = open_dir(DIRECTORY, NAME)
+    searcher = ix.searcher()
+    qp = MultifieldParser(fieldnames=['title', 'author', 'tags', 'notes', 'text'],
+                          fieldboosts={'title': 5,
+                                       'author': 5,
+                                       'tags': 3,
+                                       'notes': 2,
+                                       'text': 1},
+                          schema=ix.schema)
+
+    while True:
+        q = sys.stdin.readline().rstrip()
+        if not q:
+            break
+        print '<query %r>' % q
+
+        for hit in searcher.search(qp.parse(unicode(q.decode('utf8'))), limit=5):
+            print hit['title']
+            print hit['cached'] + '.d/notes.org'
+            print hit['tags']
+            print
 
 
 def ack(*x):
@@ -55,29 +82,39 @@ def search(*q):
     Search skid-marks plain-text or metadata.
     """
     q = ' '.join(q)
-    print
-    print 'query:', q
+    print yellow % 'query: %r showing to 10 results' % (q,)
+
+    def link(x):
+        if not x.startswith('http') and not x.startswith('file://'):
+            # add file:// prefix if file, http other wise
+            return 'file://' + x
+        return x
+
+    def author(x):
+        if not x.strip():
+            return ''
+        last = [a.strip().split()[-1] for a in x.split(';')]
+        if len(last) == 1:
+            return '(%s)' % last[0]
+        elif len(last) == 2:
+            return '(%s & %s)' % (last[0], last[1])
+        else:
+            return '(%s, %s et al.)' % (last[0], last[1])
 
     for hit in index.search(q):
-        fields = ['title', 'author', 'cached',
-                  #'source',
-                  #'tags'
-                  ]
-        for k in fields:
-            val = hit[k].strip()
-            if val and k != 'text':
-                if k in ('source', 'cached'):
-                    # add file:// prefix if file, http other wise
-                    if not val.startswith('http') and not val.startswith('file://'):
-                        val = 'file://' + val
-                if k == 'cached':
-                    # link to the notes file.
-                    print '%s: %s' % (red % 'notes', cyan % (val + '.d/notes.org'))
-                    # link to directory
-#                    print '%s: %s' % (red % 'd', cyan % (val + '.d/'))
-                if k in ('cached', 'source'):
-                    val = cyan % val   # color 'links'
-                print ('%s: %s' % (red % k, val.replace('\n', ' '))).encode('utf8')
+        a = author(hit['author'])
+        if a:
+            x = '%s %s' % (magenta % a, hit['title'])
+        else:
+            x = hit['title']
+        print x.strip().replace('\n', ' ').encode('utf8')
+
+        if hit['source'].startswith('http'):
+            print cyan % link(hit['source'])
+        else:
+            print cyan % link(hit['cached'])
+
+        print cyan % link(hit['cached'] + '.d/notes.org')
         print
     print
 
@@ -98,7 +135,7 @@ def search_org(*q):
         print ('\n+ %s' % hit['title']).encode('utf8')
         if hit['author']:
             print ('  ' + ' ; '.join('[[skid:author:"{0}"][{0}]]'.format(x.strip()) for x in hit['author'].split(';'))).encode('utf8')
-        print ('  [[%s][directory]] [[%s][source]] [[%s][cache]] [[%s][notes]]' % (d, source, cached, notes)).encode('utf8')
+        print ('  [[%s][directory]] | [[%s][source]] | [[%s][cache]] | [[%s][notes]]' % (d, source, cached, notes)).encode('utf8')
         if hit['tags']:
             print ' ', ' '.join('[[skid:tags:%s][%s]]' % (x,x) for x in hit['tags'].split()).encode('utf8').strip()
 
@@ -106,6 +143,7 @@ def search_org(*q):
 def search1(*q):
     sys.stdout = f = file('/tmp/foo', 'wb')
     search_org(*q)
+    sys.stdout.flush()
     os.system("emacs -nw /tmp/foo -e 'org-mode'")
     sys.stdout = sys.__stdout__
 
