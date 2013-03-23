8660771a7db0fcaaf52966a045ca76f12da1b42c
timv <tim.f.vieira@gmail.com>
2013-03-23 16:23:23 -0400

    ()
    --HG--
    rename : sandbox/explore/main.py => sandbox/explore/mds.py

---
+++ utils/patch-date-added.py
@@ -0,0 +1,18 @@
+"""
+Quick fix to add an approximation (mtime) to date added before we tracked it.
+"""
+
+from skid import config
+from skid.add import Document
+
+from datetime import datetime
+
+for f in config.CACHE.files():
+
+    d = Document(f)
+
+    mtime = (f + '.d').ctime
+    mtime = str(datetime.fromtimestamp(mtime))
+
+    # won't overwrite
+    d.store('data/date-added', mtime, overwrite=False)
