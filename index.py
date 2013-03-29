578e98f7d6cf050796c5a2ff6421adc783f13bd9
timv <tim.f.vieira@gmail.com>
2013-03-28 20:31:05 -0400

    tweaks. might have finally fixed issues with stop words in the query parser.

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, KEYWORD, ID, DATETIME
 from whoosh.qparser import QueryParser, MultifieldParser
-#from whoosh.analysis import StandardAnalyzer
+from whoosh.analysis import StandardAnalyzer, SimpleAnalyzer, KeywordAnalyzer, STOP_WORDS
 
 from skid.config import ROOT, CACHE
 from skid.add import Document
@@ -17,6 +17,7 @@ from skid.add import Document
 # globals
 DIRECTORY = ROOT + '/index'
 NAME = 'index'
+
 
 def create():
     """ Create a new Whoosh index.. """
@@ -31,7 +32,7 @@ def create():
                     year = TEXT(stored=True),
                     notes = TEXT(stored=True),
                     text = TEXT(stored=True),
-                    tags = KEYWORD(stored=True),
+                    tags = TEXT(stored=True, analyzer=KeywordAnalyzer()),
                     added = DATETIME(stored=True),
                     mtime = DATETIME(stored=True))
     create_in(DIRECTORY, schema, NAME)
@@ -48,26 +49,13 @@ def search(q, limit=10):
                                            'notes': 2,
                                            'text': 1},
                               schema=ix.schema)
-        # pass query thru standard analyzer or else Whoosh will choke on stopwords
-#        q = u' '.join(tk.text for tk in StandardAnalyzer()(q))   # this fails on attributes, e.g. "author:vieira"
-#        from arsenal.debug import ip; ip()
 
-        # expect user to use 'AND' and 'OR' for conjunctive/disjunctive queries
-        q = re.sub(r'\b(and|or)\b', '', q) # remove 'and' and 'or'
+        # Whoosh chokes on queries with stop words, so remove them.
+        q = re.sub(r'\b(%s)\b' % '|'.join(STOP_WORDS), '', q)
 
         q = qp.parse(q)
         for hit in searcher.search(q, limit=limit):
             yield hit
-
-
-#def search2(q, limit=10):
-#    q = unicode(q.decode('utf8'))
-#    ix = open_dir(DIRECTORY, NAME)
-#    with ix.searcher() as searcher:
-#        qp = QueryParser('text', schema=ix.schema)
-#        q = qp.parse(q)
-#        for hit in searcher.search(q, limit=limit):
-#            yield hit
 
 
 #def correct(qstr):
@@ -110,59 +98,51 @@ def update():
     # get handle to Whoosh index
     ix = open_dir(DIRECTORY, NAME)
 
-    # TODO: quicker ways to update modified files: sort files by mtime; stop
-    # updating at the first unchanged file.
     with ix.writer() as w, ix.searcher() as searcher:
 
         # sort cached files by mtime.
-        files = list(CACHE.files())
-        files.sort(key = (lambda x: (x + '.d').mtime), reverse=True)
+        files = [Document(f) for f in CACHE.files()]
+        files.sort(key = (lambda x: x.modified), reverse=True)
 
-        for cached in files:
-
-            d = Document(cached)
-
-            # mtime of directory, not the cached file
-            mtime = datetime.fromtimestamp(d.d.mtime)
+        for d in files:
 
             # lookup document mtime in the index; don't add or extract info if
             # you don't need it.
-            result = searcher.find('cached', unicode(cached))
+            result = searcher.find('cached', unicode(d.cached))
 
             if not result:
-                print '[INFO] new document', cached
+                print '[INFO] new document', d.cached
 
             else:
                 assert len(result) == 1, 'cached should be unique.'
                 result = result[0]
-                if mtime <= result['mtime']:   # already up to date
+                if d.modified <= result['mtime']:   # already up to date
 
                     # Since we've sorted files by mtime, we know that files
                     # after this one are older, and thus we're done.
                     return
 
-                print '[INFO] update to existing document:', cached
-
-            text = d.text()
+                print '[INFO] update to existing document:', d.cached
 
             meta = d.parse_notes()
 
-            assert meta['cached'] == cached, \
+            # just a lint check
+            assert meta['cached'] == d.cached, \
                 'Cached field in notes (%s) ' \
                 'does not match associated file (%s) ' \
                 'in notes file %r' % (meta['cached'],
-                                      cached,
+                                      d.cached,
                                       'file://' + d.d/'notes.org')
 
             w.update_document(source = meta['source'],
-                              cached = unicode(cached),
+                              cached = unicode(d.cached),
                               hash = d.hash(),
                               title = meta['title'],
                               author = meta.get('author', u''),
                               year = meta.get('year', u''),
                               notes = meta['notes'],
-                              text = text,
-                              mtime = mtime,
+                              text = d.text(),
+                              mtime = d.modified,
                               added = d.added,
                               tags = meta['tags'])
 
